f79ce82a6c9dbac3e5440a3b9552128a313ea0e2
phunglq99 <phunglai1999@gmail.com>
2023-07-26 17:52:58 +0700

    backend 26/7

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,23 +1,24 @@
-import { PostModel } from "../models/PostModel.js";
+import { PostModel } from '../models/PostModel.js';
 
 export const getPosts = async (req, res) => {
     try {
         const posts = await PostModel.find();
-        console.log('posts',posts);
+        console.log('posts', posts);
         res.status(200).json(posts);
-    } catch(err) {
-        res.status(500).json({error: err});
+    } catch (err) {
+        res.status(500).json({ error: err });
     }
 };
 
 export const createPost = async (req, res) => {
     try {
         const newPost = req.body;
+        newPost.title = newPost.title || 'Default Title';
         const posts = new PostModel(newPost);
         await posts.save();
 
         res.status(200).json(posts);
-    } catch(err) {
+    } catch (err) {
         console.log('err: ', err);
     }
 };
@@ -26,11 +27,12 @@ export const updatePost = async (req, res) => {
     try {
         const updatePost = req.body;
         const posts = await PostModel.findByIdAndUpdate(
-            {_id: updatePost._id}, updatePost, {new: true}
-        ) //updatePost: dữ liệu muốn update
+            { _id: updatePost._id },
+            updatePost,
+            { new: true }
+        ); //updatePost: dữ liệu muốn update
         res.status(200).json(posts);
-
-    } catch(err) {
+    } catch (err) {
         console.log('err: ', err);
     }
 };
